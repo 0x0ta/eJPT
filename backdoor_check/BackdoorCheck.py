72f53d1aa2595b47169940a2dd582c646701bc07
0x0ta <hayley.samantha86@gmail.com>
2022-04-12 02:22:08 +0100

    updated BackdoorChech to be PEP8 compliant

--- backdoor_check/BackdoorCheck.py
+++ backdoor_check/BackdoorCheck.py
@@ -1,16 +1,17 @@
 import requests as req
-import time
+
 
 def main():
-	print("*"*40)
-	server = input("Enter IP/Host to scan: ")
-	print(f'Scanning: {server} for known backdoor webpages.')
-	print("*"*40)
-	with open('BackdoorList.txt', encoding='utf8', errors='ignore') as f:
-		for x in f.readlines():
-			x = x.strip('\n\t\r')
-			resp = req.request('GET', f'https://{server}/{x}')
-			if resp.status_code == 200:
-				print(f"{x}: Found on {server} @ {server}/{x}")
-				
+    print("*" * 40)
+    server = input("Enter IP/Host to scan: ")
+    print(f'Scanning: {server} for known backdoor webpages.')
+    print("*" * 40)
+    with open('BackdoorList.txt', encoding='utf8', errors='ignore') as f:
+        for x in f.readlines():
+            x = x.strip('\n\t\r')
+            resp = req.request('GET', f'https://{server}/{x}')
+            if resp.status_code == 200:
+                print(f"{x}: Found on {server} @ {server}/{x}")
+
+
 main()
