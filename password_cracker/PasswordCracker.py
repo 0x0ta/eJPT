63c37cbd98e4d85baf8e118f85e744cd6376c842
0x0ta <hayley.samantha86@gmail.com>
2022-04-12 09:57:57 +0100

    Updated to be PEP-8 Compliant

--- password_cracker/PasswordCracker.py
+++ password_cracker/PasswordCracker.py
@@ -2,22 +2,25 @@ import http.client
 import urllib.parse
 import os
 
-def main():
-	usernamelist = input("Enter location of username list: ")
-	passwordlist = input("Enter location of password lsit: ")
-	attack_uri = input("Enter url to test: ")
-	if (os.path.exists(usernamelist) != False):
-		if(os.path.exists(passwordlist) != False):
-			with open(usernamelist, "r") as F:
-				for x in usernamelist.readlines():
-					x = x.strip('\r\t\n')
-					if (attack_uri.__contains__("https://") == False):
-						print(f"Error: {attack_uri} does not contain https://")
 
-		else:
-			print(f"Error: {passwordlist} does not exist, please check the input and try again.")
-	else:
-		print(f"Error: {usernamelist} does not exist, please check input and try again.")
+def main():
+    usernamelist = input("Enter location of username list: ")
+    passwordlist = input("Enter location of password lsit: ")
+    attack_uri = input("Enter url to test: ")
+    if (os.path.exists(usernamelist) is not False):
+        if(os.path.exists(passwordlist) is not False):
+            with open(usernamelist, "r") as f:
+                for x in usernamelist.readlines():
+                    x = x.strip('\r\t\n')
+                    if (attack_uri.__contains__("https://") is False):
+                        print(f"Error: {attack_uri} does not contain https://")
+
+        else:
+            print(
+                f"Error: {passwordlist} does not exist, please check the input and try again.")
+    else:
+        print(
+            f"Error: {usernamelist} does not exist, please check input and try again.")
 
 
 main()
